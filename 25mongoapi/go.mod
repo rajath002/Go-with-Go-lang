5d01981783c7da154cb95f4b4a5d6978c3e3964d
Rajath <acharya.drajath@gmail.com>
2023-03-27 18:06:12 +0530

    24: integrated Mongodb, started creating Models (netflix)

---
+++ 25mongoapi/go.mod
@@ -0,0 +1,5 @@
+module github.com/rajath002/gomongoapi
+
+go 1.20
+
+require go.mongodb.org/mongo-driver v1.11.3 // indirect
---
+++ 25mongoapi/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go mongo API")
+
+	//mongodb+srv://admin:<password>@cluster-demo-project.hahillw.mongodb.net/?retryWrites=true&w=majority
+}
