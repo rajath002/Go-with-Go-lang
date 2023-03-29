792c366a6e3c36937bf90726affb19b6028e74f4
Rajath <acharya.drajath@gmail.com>
2023-03-29 12:34:31 +0530

    51 router added

--- 25mongoapi/main.go
+++ 25mongoapi/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/rajath002/gomongoapi/router"
+)
 
 func main() {
 	fmt.Println("Go mongodb API")
 
+	r := router.Router()
+	fmt.Println("Server is getting started...")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000")
 	//mongodb+srv://admin:<password>@cluster-demo-project.hahillw.mongodb.net/?retryWrites=true&w=majority
 }
